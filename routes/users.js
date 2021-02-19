8cc0c111ad9d67ecd1d3e992fce0e65e2f71afe2
rizky-sudo <agung.tech06@gmail.com>
2021-02-19 19:46:58 +0700

    fix user.js

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,19 @@ module.exports = function (pool) {
   });
 
   router.put('/:iduser', function (req, res, next) {
-pool.query()
-  })
+    pool.query('UPDATE public.user SET nama=$1, email=$2, password=$3, nohandphone=$4 WHERE iduser=$5', [req.body.nama, req.body.email, req.body.password, req.body.nohandphone, NUMBER(req.body.iduser)], (err, data) => {
+      if (err) throw err;
+      res.json(data.rows);
+    })
+  });
 
+  router.delete('\:iduser', function (req, res, next) {
+    pool.query('DELETE FROM public.user WHERE iduser=$1', [Number(req.params.iduser)], (err, data) => {
+      if (err) throw err;
+      res.json(data.rows);
+    })
+  });
 
-
-
+  return router;
 
 }
-
-module.exports = router;
