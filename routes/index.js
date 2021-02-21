32ab7eff1a517651cc40a97e516199ef249a3da9
rizky-sudo <agung.tech06@gmail.com>
2021-02-21 20:09:22 +0700

    signin not yet fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { query } = require('express');
 const saltRound = 10
 
 // middleware login 
@@ -65,7 +66,56 @@ module.exports = function (pool) {
             sql += `${field[i]}`;
             break;
         }
+        if (i !== field.length - 1) sql += `AND`;
       }
     }
-  })
+    if (filter.length2 < 2) {
+      sql += ` ORDER BY idiklan ASC`;
+    }
+    sql += ` LIMIT ${perpage} OFFSET ${offset}`;
+    console.log(sql);
+
+    pool.query(sql, (err, data) => {
+      if (err) throw err;
+
+      res.render('index', {
+        data: data.rows,
+        query: queries,
+        current: page,
+        pages,
+        url,
+        user: res.session.user
+      });
+      console.log(data.rows.length);
+    });
+  });
 }
+
+// sign in
+router.get('/signin', function (req, res, next) {
+  pool.query('SELECT * FROM public.user WHERE email = $1', [req.body.email], (err, data) => {
+    if (err) {
+      req.flash('info', "try again later!");
+      return res.redirect('/signin');
+    }
+    if (data.rows.length == 0) {
+      req.flash('info', "email does't exist!");
+      return res.redirect('signin');
+    }
+    bcrypt.compare(req.body.password, data.rows[0].password, function (err, result) {
+      if (err) {
+        req.flash('info', "username or password does't exits!");
+        return res.redirect('/signin');
+      }
+      if (result) {
+        let user = data.rows[1];
+        delete user['password'];
+        req.session.user = user;
+        res.redirect('/');
+      } else {
+        req.flash('info', "username or password does't exist!");
+        return res.redirect('/signin');
+      }
+    });
+  });
+});
