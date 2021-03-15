9b32afe4df197155a2a7fb7f74845fd9d879f415
rizky-sudo <agung.tech06@gmail.com>
2021-03-15 10:43:21 +0700

    fix script.js

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+// Google Maps Script API
+
+let map;
+// Center point Indonesia
+map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+        lat: -3.334465038429288,
+        lng: 124.09884761182217
+    },
+    zoom: 4.5
+});
